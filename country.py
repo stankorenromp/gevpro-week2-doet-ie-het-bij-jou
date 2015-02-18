aff729ddef50c34aea07ab9a1abf99f726a11280
robertveenhoven <robert-veenhoven@hotmail.com>
2015-02-18 01:12:47 -0800

    Newlines in lijst weggehaald en goed geindenteerd

--- country.py
+++ country.py
@@ -1,17 +1,19 @@
 class Country:
 
-    def __init__(self, name):
-        self.name=name
+	def __init__(self, name):
+		self.name=name
 
-    def __str__(self):
-        return 'Hello from ' + self.name 
+	def __str__(self):
+		return 'Hello from ' + self.name 
         
-    def readcountry():
-    	file = open('countries_list.txt', 'r')
-    	countrylist = []
-    	for country in file:
-    		countrylist.append(country)
-        return countrylist
+def readcountry():
+	file = open('countries_list.txt', 'r')
+	countrylist = []
+	for country in file:
+		strippedcountry=country.strip("\n")
+		countrylist.append(strippedcountry)
+	
+	return countrylist
         
 def main():
 	countrylist = readcountry()
